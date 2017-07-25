5d27df5316e2923250ba1467dd22951156cfdc89
SeatSpnos <segmento.especializado@gmail.com>
2017-07-25 13:56:08 +0100

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const controllers = require('../controllers');
 
 module.exports = function (app) {
   app.get('/allOt/:ot', controllers.test.find.allOt);
-  app.get('/list/', controllers.test.find.list);
-  app.get('/items/', controllers.test.find.items);  
+  app.get('/list', controllers.test.find.list);
+  app.get('/items', controllers.test.find.items);
   app.post('/items', controllers.test.insert.items);
   app.post('/numAndSerial', controllers.test.insert.numAndSerial);
   app.post('/list', controllers.test.insert.list);
