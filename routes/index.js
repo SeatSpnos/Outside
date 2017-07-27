77bbe12a268d1df7cf42d95b9566b2570ce23a96
SeatSpnos <segmento.especializado@gmail.com>
2017-07-27 14:53:30 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,5 @@ module.exports = function (app) {
   app.use('/login', require('./auth'));
   app.use('/test', isLoggedIn, require('./test'));
   app.get('/', controllers.init);
-  app.use('./API', require('./API'));
+  app.use('/API', require('./API'));
 };
