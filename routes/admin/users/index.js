beb5388ea5469edfe98040509bf8100b971dc44c
SeatSpnos <segmento.especializado@gmail.com>
2017-07-27 12:44:10 +0100

    Delete index.js

--- routes/admin/users/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-const controllers = require('controllers');
-
-router.get('/users', controllers.admin.users.find.all);
-router.get('/users/:id', controllers.admin.users.find.byId);
-router.get('/:username', controllers.admin.users.find.byUsername);
-router.post('/users', controllers.admin.users.insert);
-module.exports = router;
