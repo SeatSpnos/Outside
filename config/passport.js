9da9bdb99b8108c872fdff627e9b90885a5ab3ed
SeatSpnos <segmento.especializado@gmail.com>
2017-07-27 17:33:00 +0100

    Add files via upload

--- config/passport.js
+++ config/passport.js
@@ -22,13 +22,12 @@ module.exports = function (passport) {
     },
     (req, username, password, done) => {
       userModel.findOne({username: username}, function (err, user) {
-        console.log(err);
-        console.log(user);
         if (err) return done(err);
         if (!user) {
           return done(null, false, req.flash('loginMessage', 'Esse utilizador não foi encontrado.'));
         }
         if (!bcrypt.compareSync(password, user.password)) {
+          console.log('Wrong password');
           return done(null, false, req.flash('loginMessage', 'Oops! Password Incorrecta.'));
         }
         done(null, user);
