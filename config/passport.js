14a38bd6d4cd6809b870086f38b6e77710b430fd
SeatSpnos <segmento.especializado@gmail.com>
2017-07-27 16:36:52 +0100

    Add files via upload

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,39 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt-nodejs');
 const userModel = require('../models').user;
-const login = 'local-login';
-const strategy = new LocalStrategy({
-  usernameField: 'username',
-  passwordField: 'password',
-  passReqToCallback: true
-}, verification);
 
 module.exports = function (passport) {
-  passport.serializeUser(function (user, done) {
+  passport.serializeUser((user, done) => {
     done(null, user.id);
   });
 
-  passport.deserializeUser(function (id, done) {
-    userModel.selectID(id, function (err, rows) {
-      done(err, rows[0]);
+  passport.deserializeUser((id, done) => {
+    userModel.find({id: id}, function (err, user) {
+      done(err, user);
     });
   });
 
-  passport.use(login, strategy);
+  passport.use(
+    'local-login',
+    new LocalStrategy({
+      usernameField: 'username',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    (req, username, password, done) => {
+      return done(null, {id: '_312312'}) 
+      userModel.find({username: username}, function (err, user) {
+        console.log(err);
+        console.log(user);
+        if (err) return done(err);
+        if (!user) {
+          return done(null, false, req.flash('loginMessage', 'Esse utilizador não foi encontrado.'));
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false, req.flash('loginMessage', 'Oops! Password Incorrecta.'));
+        }
+        done(null, user);
+      });
+    })
+  );
 };
-
-function verification (req, username, password, done) {
-  userModel.find({username: username}, function (err, user) {
-    console.log(user);
-    if (err) return done(err);
-    if (!user.length) return done(null, false, req.flash('loginMessage', 'Esse utilizador não foi encontrado.'));
-    if (!bcrypt.compareSync(password, user.password)) return done(null, false, req.flash('loginMessage', 'Oops! Password Incorrecta.'));
-    done(null, user);
-  });
-}
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,20 @@ const controllers = require('../controllers');
 const middlewares = require('../middlewares');
 const isLoggedIn = middlewares.isLoggedIn;
 
-module.exports = function (app) {
+module.exports = function (app, passport) {
   app.get('/', controllers.init);
-  app.use('/login', require('./auth'));
+  app.get('/login', controllers.auth.init);
+  app.post('/login', passport.authenticate('local-login', {
+    sucessRedirect: '/test',
+    failureRedirect: '/test',
+    failureFlash: true
+  }),
+   (req, res) => {
+    req.body.remember
+    ? req.session.cookie.maxAge = 1000 * 60 * 60 * 4
+    : req.session.cookie.expires = false;
+    res.redirect('/test');
+  });
   app.use('/test', isLoggedIn, require('./test'));
   app.use('/API', require('./API'));
 };
