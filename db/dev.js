ffdc2af992ae77deccc5edd8ef6d5a3d52a9cbf7
SeatSpnos <segmento.especializado@gmail.com>
2017-07-27 12:43:27 +0100

    Delete dev.js

--- db/dev.js
+++
@@ -1,5 +0,0 @@
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://<test>:<test123>@ds119533.mlab.com:19533/seat_hosting';
-mongoose.connect(mongoDB);
-
-module.exports = mongoose.connection;
