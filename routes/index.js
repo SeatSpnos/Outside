73a9dab37d1283cafe42fdb9ef381f1d0232a9bf
SeatSpnos <segmento.especializado@gmail.com>
2017-07-27 14:56:19 +0100

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const middlewares = require('../middlewares');
 const isLoggedIn = middlewares.isLoggedIn;
 
 module.exports = function (app) {
+	app.get('/', controllers.init);
   app.use('/login', require('./auth'));
-  app.use('/test', isLoggedIn, require('./test'));
-  app.get('/', controllers.init);
+  app.use('/test', isLoggedIn, require('./test'));  
   app.use('/API', require('./API'));
 };
